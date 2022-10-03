718291aedea3a6f81deaa534c77d00d505912e83
Fatma <fmohamed34@gmail.com>
2022-10-03 11:27:38 -0400

    Completed example functions

---
+++ functions/simple_functions.js
@@ -0,0 +1,19 @@
+function favoriteThings () {
+console.log (Fatma Mohamed);
+console.log (My favorite movie is "White Chicks"!);
+console.log (I love listening to HipHop and R&B.);
+}
+
+function whyImHere () {
+    console.log (I am here to change my life and create better opportunities for my family.);
+}
+
+function favoritePlace () {
+    console.log (My favorite place in the world is at home with my husband and kids.);
+}
+
+// calling for my functions
+
+favoriteThings ();
+whyImHere ();
+favoritePlace ();
